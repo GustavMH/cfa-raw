2263567b2a8fab4cbd8f12ced1a6bbc0b073b0ba
Gustav <gustav@ledning.org>
2024-02-26 15:28:24 +0100

    create dataset prep notebook

--- notebooks/dng.py
+++ notebooks/dng.py
@@ -9,6 +9,7 @@ ccm1 = [[10000,10000], [10000,10000], [10000,10000],
         [10000,10000], [10000,10000], [10000,10000]]
 
 # Test Colour Correction Matrix - 2
+# Leads to errors
 ccm1 = [[0,1], [0,1], [0,1],
         [0,1], [0,1], [0,1],
         [0,1], [0,1], [0,1]]
@@ -18,21 +19,25 @@ def saveFile(cfa_img, filename, bpp = 16):
 
     # set DNG tags.
     t = DNGTags()
-    t.set(Tag.ColorMatrix1, ccm1) # colour correction matrix
+    #t.set(Tag.ColorMatrix1, ccm1) # colour correction matrix
 
     t.set(Tag.ImageWidth, width)
     t.set(Tag.ImageLength, height)
-    t.set(Tag.TileWidth, width)
-    t.set(Tag.TileLength, height)
+    #t.set(Tag.TileWidth, width) # Not supported?
+    #t.set(Tag.TileLength, height) # Not supported?
     t.set(Tag.Orientation, Orientation.Horizontal)
     t.set(Tag.PhotometricInterpretation, PhotometricInterpretation.Color_Filter_Array)
     t.set(Tag.SamplesPerPixel, 1)
     t.set(Tag.BitsPerSample, bpp)
     t.set(Tag.CFARepeatPatternDim, [2,2])
     t.set(Tag.CFAPattern, CFAPattern.GBRG)
-    t.set(Tag.BlackLevel, (4096 >> (16 - bpp)))
-    t.set(Tag.WhiteLevel, ((1 << bpp) -1) )
-    t.set(Tag.CalibrationIlluminant1, CalibrationIlluminant.D65)
+    #t.set(Tag.BlackLevel, (4096 >> (16 - bpp))) # def. 0
+    #t.set(Tag.WhiteLevel, ((1 << bpp) -1)) # def. 2^bpp -1
+
+    #t.set(Tag.CalibrationIlluminant1, CalibrationIlluminant.D65) # def. unknown
+    t.set(Tag.AsShotNeutral, [[1,1],[1,1],[1,1]]) # RGB white
+    #t.set(Tag.BaselineExposure, [[1, 0]]) # Exposure compensation, defualt 0, repr. 0/1?
+
     t.set(Tag.Make, "No camera - Synthetically generated")
     t.set(Tag.Model, "v1")
     t.set(Tag.DNGVersion, DNGVersion.V1_4)
@@ -41,5 +46,5 @@ def saveFile(cfa_img, filename, bpp = 16):
 
     # save to dng file.
     r = RAW2DNG()
-    r.options(t, path="", compress=False)
+    r.options(t, path="", compress=False) # Enabling compression crashes Jupyter
     r.convert(cfa_img, filename=filename)
