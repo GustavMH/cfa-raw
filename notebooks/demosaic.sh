81c3e23abc58a39df678198e43217a702ea147d9
Gustav <gustav@ledning.org>
2024-03-21 21:58:52 +0100

    Fixed needing to compile pnmtopng

--- notebooks/demosaic.sh
+++ notebooks/demosaic.sh
@@ -11,7 +11,7 @@ i=$((2 * n_threads))
 time (
 for path in $(cd dng && find -type f | sed "s/\.\///;s/\.dng//"); do
    ((i=i%$((2*n_threads)))); ((i++==0)) && wait
-   (dcraw -q 3 -m 3 -c dng/$path.dng | pnmtopng > ahd/$path.png) &
+   (dcraw -q 3 -m 3 -T dng/$path.dng) && mv dng/$path.tiff ahd/$path.tiff &
 done
 )
 wait
