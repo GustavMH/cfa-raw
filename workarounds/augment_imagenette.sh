356215bef10443a7c8fd74a4d381af4265f1124c
Gustav <gustav@ledning.org>
2024-04-14 09:03:36 +0200

    removed dupe file

--- workarounds/augment_imagenette.sh
+++ workarounds/augment_imagenette.sh
@@ -10,4 +10,4 @@ lscpu
 
 echo "STARTING DATASET AUGMENTATION"
 
-python3.11 cfa-raw/notebooks/augment_dataset.py --input /home/zvq211/imagenette2-160 --output /home/zvq211/ds_160 --size 160 10 30 70
+python3.11 cfa-raw/notebooks/augment_dataset.py --input /home/zvq211/imagenette2-160 --output /home/zvq211/ds_160_log --size 160 64 32.0 16.0 8.0 4.0 2.0 1.0 0.5 0.25 0.125 0.0625
--- workarounds/make_scripts.py
+++ workarounds/make_scripts.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
-
 from itertools import product
 
 forms = [("npy","npy"), ("ahd","tiff"), ("png","png")]
 noise_types = ["gaussian", "speckle", "saltpepper"]
-intensities = ["10", "30", "70"]
+intensities = [
+    '64pct', '32.0pct', '16.0pct', '8.0pct',
+    '4.0pct', '2.0pct', '1.0pct', '0.5pct',
+    '0.25pct', '0.125pct', '0.0625pct'
+]
+ds_path = "/home/zvq211/ds_160_log"
+model_path = "/home/zvq211/models"
 
-def write_script(form, ext, noise_type, intensity):
-    filename = f"{form}{noise_type}{intensity}.sh"
+
+def write_script(form, ext, noise_type, intensity, n):
+    filename = f"{form}{noise_type}{intensity}_{n}.sh"
     with open(filename, "w") as f:
         f.write("#!/usr/bin/env bash\n")
-        f.write("#SBATCH --ntasks=1 --gres=gpu:titanrtx --cpus-per-task=8 --mem=22000M --time=10:00:00 \n")
-        f.write("source /home/zvq211/cfa-raw/.venv/bin/activate\n")
-        f.write(f"python3.11 /home/zvq211/cfa-raw/notebooks/train.py --clean /home/zvq211/ds_160/png/none/0pct/ --noise /home/zvq211/ds_160/{form}/{noise_type}/{intensity}pct/ --output /home/zvq211/models --model '/home/zvq211/models/{form}{noise_type}{intensity}-model.pkl' --name {form}{noise_type}{intensity} --type .{ext}\n")
+        f.write("#SBATCH --ntasks=1 --gres=gpu:titanrtx --cpus-per-task=8 --mem=8000M --time=10:00:00 \n")
+        f.write("source cfa-raw/.venv/bin/activate\n")
+        f.write(f"python3.11 cfa-raw/notebooks/train.py")
+        f.write(f"--clean {ds_path}/png/none/0pct/ ")
+        f.write(f"--noise {ds_path}/{form}/{noise_type}/{intensity}pct/ ")
+        f.write(f"--output {model_path} ")
+        f.write(f"--model {model_path}/{form}{noise_type}{intensity}_{n}-model.pkl")
+        f.write(f"--name {form}{noise_type}{intensity}_{n} ")
+        f.write(f"--type .{ext} ")
+        f.write(f"--epochs 100\n")
 
 for (form, ext), noise_type, intensity in product(forms, noise_types, intensities):
-    write_script(form, ext, noise_type, intensity)
+    for n in range(5):
+        write_script(form, ext, noise_type, intensity, n)
