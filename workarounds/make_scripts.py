ace7c225916ac603c93f740afef2e9bf2e9a08e4
Gustav <gustav@ledning.org>
2024-04-14 09:41:13 +0200

    rewritten make_scripts

--- workarounds/make_scripts.py
+++ workarounds/make_scripts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from itertools import product
+from pathlib   import Path
 
 forms = [("npy","npy"), ("ahd","tiff"), ("png","png")]
 noise_types = ["gaussian", "speckle", "saltpepper"]
@@ -8,26 +9,33 @@ intensities = [
     '4.0pct', '2.0pct', '1.0pct', '0.5pct',
     '0.25pct', '0.125pct', '0.0625pct'
 ]
-ds_path = "/home/zvq211/ds_160_log"
-model_path = "/home/zvq211/models"
+home       = Path("/home/zvq211")
+ds_path    = home / "ds_160_log"
+model_path = home / "models"
+prog_path  = home / "cfa-raw" / "notebooks" / "train.py"
 
 
 def write_script(form, ext, noise_type, intensity, n):
-    filename = f"{form}{noise_type}{intensity}_{n}.sh"
+    name     = f"{form}{noise_type}{intensity}_{n}"
+    filename = f"{name}.sh"
+    commands = [
+        ["#!/usr/bin/env bash"],
+        ["#SBATCH --ntasks=1 --gres=gpu:titanrtx --cpus-per-task=8 --mem=8000M --time=10:00:00"],
+        [f"source {home / 'cfa-raw'}/.venv/bin/activate"],
+        [
+            f"python3.11 {prog_path}"
+            f"--clean {ds_path}/png/none/0pct/",
+            f"--noise {ds_path}/{form}/{noise_type}/{intensity}pct/",
+            f"--output {model_path}",
+            f"--model {model_path}/{name}-model.pkl",
+            f"--name {name}",
+            f"--type .{ext}",
+            f"--epochs 100"
+        ]
+    ]
+
     with open(filename, "w") as f:
-        # This section should be rewritten
-        # with pathlib and a list of args so whitespace is automatic
-        f.write("#!/usr/bin/env bash\n")
-        f.write("#SBATCH --ntasks=1 --gres=gpu:titanrtx --cpus-per-task=8 --mem=8000M --time=10:00:00 \n")
-        f.write("source cfa-raw/.venv/bin/activate\n")
-        f.write(f"python3.11 cfa-raw/notebooks/train.py ")
-        f.write(f"--clean {ds_path}/png/none/0pct/ ")
-        f.write(f"--noise {ds_path}/{form}/{noise_type}/{intensity}pct/ ")
-        f.write(f"--output {model_path} ")
-        f.write(f"--model {model_path}/{form}{noise_type}{intensity}_{n}-model.pkl")
-        f.write(f"--name {form}{noise_type}{intensity}_{n} ")
-        f.write(f"--type .{ext} ")
-        f.write(f"--epochs 100\n")
+        f.write("\n".join([" ".join(args) for args in commands]))
 
 for (form, ext), noise_type, intensity in product(forms, noise_types, intensities):
     for n in range(5):
