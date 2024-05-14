28d7d0b3a1ff9e20a477d890bd95122cf5aee2b5
Gustav <gustav@ledning.org>
2024-05-14 12:16:24 +0200

    fix IFS problem

--- workarounds/demosaic_0noise.sh
+++ workarounds/demosaic_0noise.sh
@@ -1,31 +1,41 @@
 #!/usr/bin/env bash
 #SBATCH --ntasks=1 --cpus-per-task=12 --time=10:00:00 --mem=6000M
 
-export ds_path=/home/zvq211/ds_160_log
+export ds_path=~/Downloads/ds_proc #/home/zvq211/ds_160_log
 export PATH=/home/zvq211/cfa-raw/dcraw:$PATH
 
 n_threads=$(eval nproc)
 
 cd $ds_path
 
+# copy folder structure
 for j in "mw,-m 3" "m,-m 3 -W", "w, ", "d, -W"; do
+   # Set the internal field splitter, reset after use
    IFS=","
    set -- $j
+   IFS=$' \t\n'
+
    for quality in 0 1 2 3; do
       (cd dng/none/0pct && find . -type d -exec mkdir -p -- ../../../$1$quality/{} \; ) && wait
    done
 done
 
-# copy folder structure
-for j in "mw,-m 3" "m,-m 3 -W", "w, ", "d, -W"; do
+# Get a path to every .dng file
+LINES=$(cd dng/none/0pct && find -type f | sed "s/\.\///;s/\.dng//")
+
+for j in "mw, -m 3" "m, -m 3 -W", "w, ", "d, -W"; do
+   # Set the internal field splitter, reset after use
    IFS=","
    set -- $j
+   IFS=$' \t\n'
    for quality in 0 1 2 3; do
       i=$((2 * n_threads))
-      for path in $(cd dng/none/0pct && find -type f | sed "s/\.\///;s/\.dng//"); do
+      for path in $LINES; do
          ((i=i%$((2*n_threads)))); ((i++==0)) && wait
-         (dcraw -q $quality -T -g 1 1 $2 dng/none/0pct/$path.dng) && mv dng/none/0pct/$path.tiff $1$quality/$path.tiff &
+         (dcraw -q $quality -T -g 1 1 $2 dng/none/0pct/$path.dng) \
+            && mv dng/none/0pct/$path.tiff $1$quality/$path.tiff &
       done
       wait
    done
 done
+
