a1597c7ff8cdc615b2bef9dadadc69671d003c2e
Gustav <gustav@ledning.org>
2024-05-15 15:20:01 +0200

    mv val *, *->val/

---
+++ workarounds/split_celebA.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+cd npy
+mkdir val
+find ./celeba_hq_256/ -type f -regex '.*\/2[0-9][0-9][0-9][0-9].npy' -exec mv {} ./val \;
+mv celeba_hq_256 train
+
+cd ../dng
+mkdir val
+find ./celeba_hq_256/ -type f -regex '.*\/2[0-9][0-9][0-9][0-9].dng' -exec mv {} ./val \;
+mv celeba_hq_256 train
+
+cd ../png
+mkdir val
+find ./celeba_hq_256/ -type f -regex '.*\/2[0-9][0-9][0-9][0-9].png' -exec mv {} ./val \;
+mv celeba_hq_256 train
