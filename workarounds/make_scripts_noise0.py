6d0d793032bfd89843e2b1a895e68cf62d2bc7e9
Gustav <gustav@ledning.org>
2024-05-31 22:47:08 +0200

    make new scripts

--- workarounds/make_scripts_noise0.py
+++ workarounds/make_scripts_noise0.py
@@ -2,33 +2,34 @@
 from pathlib   import Path
 
 home       = Path("/home/zvq211")
-ds_path    = home / "ds_160_log"
+ds_path    = home / "ds_proc"
 model_path = home / "models"
 prog_path  = home / "cfa-raw" / "notebooks" / "train.py"
 
 
-def write_script(form, ext, path, n):
-    name     = f"noise0_{path}_{n}"
+def write_script(ds, ext, path, n, epochs):
+    name     = f"{ds}_{path}_{n}"
     filename = f"{name}.sh"
     commands = [
         ["#!/usr/bin/env bash"],
         [f"source {home / 'cfa-raw'}/.venv/bin/activate"],
         [
             f"python3.11 {prog_path}",
-            f"--clean {ds_path}/png/none/0pct/",
-            f"--noise {ds_path}/{path}",
+            f"--clean {ds_path}/{ds}/png",
+            f"--noise {ds_path}/{ds}/{path}",
             f"--output {model_path}",
             f"--model {model_path}/{name}-model.pkl",
             f"--name {name}",
             f"--type .{ext}",
-            f"--epochs 300"
+            f"--epochs {epochs}"
         ]
     ]
 
     with open(filename, "w") as f:
         f.write("\n".join([" ".join(args) for args in commands]))
 
-for path in ["m", "mw", "w", "d"]:
-    for i in range(4):
-        for n in range(5):
-            write_script("ahd", "tiff", f"{path}{i}", n)
+for ds, epochs in [("CelebA", 100), ("imagenette2-160",300), ("GTSRB",100)]:
+    for path in ["a","aw","amw","m", "mw", "w", "d"]:
+        for i in range(4):
+            for n in range(5):
+                write_script(ds, "tiff", f"{path}{i}", n, epochs)
