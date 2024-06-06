correct kernel scaling

notebooks/denoisingautoencoder.py
@@ -2,50 +2,50 @@ import torch
 from   torch import nn
 
 class DenoisingAutoencoder(nn.Module):
-    def __init__(self, channels=3):
+    def __init__(self, channels=3, ks=64):
         super(DenoisingAutoencoder, self).__init__()
         # Encoder
         self.enc1 = nn.Sequential(
-            nn.Conv2d(channels, 8, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(8),
+            nn.Conv2d(channels, ks, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks),
             nn.ReLU(True))
         self.enc2 = nn.Sequential(
-            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(ks, ks*2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2),
             nn.ReLU(True))
         self.enc3 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(ks*2, ks*2**2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**2),
             nn.ReLU(True))
         self.enc4 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(ks*2**2, ks*2**3, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**3),
             nn.ReLU(True))
         self.enc5 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(ks*2**3, ks*2**3, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**3),
             nn.ReLU(True))
 
         # Decoder
         self.dec1 = nn.Sequential(
-            nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(ks*2**3, ks*2**3, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**3),
             nn.ReLU(True),
             nn.Dropout(0.5))
         self.dec2 = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1), # the 1024 comes from concatenation
-            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(2*ks*2**3, ks*2**2, kernel_size=4, stride=2, padding=1), # the 1024 comes from concatenation
+            nn.BatchNorm2d(ks*2**2),
             nn.ReLU(True))
         self.dec3 = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(2*ks*2**2, ks*2**1, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**1),
             nn.ReLU(True))
         self.dec4 = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(16),
+            nn.ConvTranspose2d(2*ks*2**1, ks*2**0, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ks*2**0),
             nn.ReLU(True))
         self.dec5 = nn.Sequential(
-            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(2*ks*2**0, channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh())
 
     def forward(self, x):
@@ -67,3 +67,10 @@ class DenoisingAutoencoder(nn.Module):
         d4 = torch.cat((d4, e1), dim=1)  # skip connection
         d5 = self.dec5(d4)
         return d5
+
+
+if __name__ == "__main__":
+    model = DenoisingAutoencoder(ks=8)
+    test = torch.rand((5,3,64,64), dtype=torch.float32)
+    res = model.forward(test)
+    print(test.shape, res.shape)